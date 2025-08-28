455be8b2b59a4b184dcd6913b2ef0efe835ef8c8
oknott14 <oknott14@outlook.com>
2025-08-28 12:21:18 -0400

    Remove Pycache

--- tests/extraction/structured_output/test_extraction_structured_output.py
+++ tests/extraction/structured_output/test_extraction_structured_output.py
@@ -11,28 +11,24 @@ def test_can_be_created_with_nothing():
 
 def test_validator_called_on_multiple_fields():
     dummy_data: Any = {
-        "patient_dob": "11/13/2000",
         "processing_date": "11/13/2024",
         "service_date": "12-12-12",
     }
 
     data_model = ExtractionOutput(**dummy_data)
 
-    assert data_model.patient_dob
     assert data_model.processing_date
     assert data_model.service_date
 
 
 def test_invalid_dates_are_none():
     dummy_data: Any = {
-        "patient_dob": "111/bb131/2000",
         "processing_date": "11s13s2024",
         "service_date": "this is not a date",
     }
 
     data_model = ExtractionOutput(**dummy_data)
 
-    assert not data_model.patient_dob
     assert not data_model.processing_date
     assert not data_model.service_date
 
